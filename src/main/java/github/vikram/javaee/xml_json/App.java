65f04485f210dd17d81893869a6e5aa5c0eeca94
Vikram Kommaraju <vkommara@cisco.com>
2015-07-10 18:11:17 -0700

    Commit project layout and directories

---
+++ src/main/java/github/vikram/javaee/xml_json/App.java
@@ -0,0 +1,13 @@
+package github.vikram.javaee.xml_json;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
